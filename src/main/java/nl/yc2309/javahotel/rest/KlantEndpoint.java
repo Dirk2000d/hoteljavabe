2611040bab8d4af131ffe5e4fdfc0bf6de8b8ee0
Bart Becker <bart.becker@outlook.com>
2023-10-09 09:12:35 +0200

    javaaccount aangemaakt

--- src/main/java/nl/yc2309/javahotel/rest/KlantEndpoint.java
+++ src/main/java/nl/yc2309/javahotel/rest/KlantEndpoint.java
@@ -1,5 +1,7 @@
 package nl.yc2309.javahotel.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,15 +24,20 @@ public class KlantEndpoint {
 	public Iterable<Klant> alleKlanten() {
 		return ks.geefAlleKlanten();
 	}
+	@GetMapping("klant/{id}")
+	public Optional<Klant> klant(@PathVariable long id) {
+		return ks.geefKlant(id);
+	}
+	
 	@PostMapping("voegklanttoe")
 	public String alleKlanten(@RequestBody Klant klant) {
 		ks.slaKlantOp(klant);
 		return "klant Toegevoegd";
 	}
 	
-	@DeleteMapping("verwijderklant/{klantid}")
-	public void verwijderKlant(@PathVariable("klantid") int klantid) {
-		ks.verwijderKlant(klantid);
+	@DeleteMapping("verwijderklant/{id}")
+	public void verwijderKlant(@PathVariable int id) {
+		ks.verwijderKlant(id);
 	}
 	
 	@PutMapping("updateklant")
